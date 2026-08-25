09c7d51676535bd571041484821495543c24ba63
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-25 11:36:10 +0330

    feat: Implement Builder class with uniform, input, and output methods; add IOObjectOptions type

--- src/builder/builder.ts
+++ src/builder/builder.ts
@@ -1,5 +1,34 @@
 import type { BuildTarget } from "../compiler/index.js";
+import type { IOObjectOptions } from "./objects/common.js";
+import { InputObject } from "./objects/input.object.js";
+import { OutputObject } from "./objects/output.object.js";
+import {
+  UniformObject,
+  type UniformObjectOptions,
+} from "./objects/uniform.object.js";
 
 export class Builder {
+  private readonly uniforms = new Set<UniformObject>();
+  private readonly inputs = new Set<InputObject>();
+  private readonly outputs = new Set<OutputObject>();
+
+  private returnAdd<T>(to: Set<T>, item: T) {
+    to.add(item);
+
+    return item;
+  }
+
+  public uniform(options: UniformObjectOptions) {
+    return this.returnAdd(this.uniforms, new UniformObject(options));
+  }
+
+  public input(options: IOObjectOptions) {
+    return this.returnAdd(this.inputs, new InputObject(options));
+  }
+
+  public output(options: IOObjectOptions) {
+    return this.returnAdd(this.outputs, new OutputObject(options));
+  }
+
   public build(target: BuildTarget): void {}
 }
---
+++ src/builder/objects/common.ts
@@ -0,0 +1,7 @@
+import type { DataType } from "../../types.js";
+
+export type IOObjectOptions = {
+  name: string;
+  type: DataType;
+  flatten?: boolean;
+};
