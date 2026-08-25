714dd5675bae0e088215045a4eac75b2a685dfc1
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-25 11:35:54 +0330

    feat: Add InputObject and OutputObject classes with IOObjectOptions constructor

---
+++ src/builder/objects/input.object.ts
@@ -0,0 +1,5 @@
+import type { IOObjectOptions } from "./common.js";
+
+export class InputObject {
+  public constructor(private options: IOObjectOptions) {}
+}
---
+++ src/builder/objects/output.object.ts
@@ -0,0 +1,5 @@
+import type { IOObjectOptions } from "./common.js";
+
+export class OutputObject {
+  public constructor(private options: IOObjectOptions) {}
+}
