81c3b710018a6c6cf0cb0796b10dc65af344e068
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-29 01:09:28 +0330

    refactor: Move `add.operation.ts` data to `types/additive.type.ts` to clear that those types can be used for both `subtraction` and `addition` operations

---
+++ src/builder/operations/types/additive.type.ts
@@ -0,0 +1,248 @@
+import type {
+  DATATYPE,
+  FloatVecDatatype,
+  INT_VEC_DATATYPE,
+  IntVecDatatype,
+  MatrixDatatype,
+  SCALAR_DATATYPE,
+  ScalarDataType,
+  UINT_VEC_DATATYPE,
+  UintVecDatatype,
+} from "../../../types.js";
+import type { OperationOutput, OperationRHS } from "../common.js";
+
+export type AdditiveCombinationResult<R, O> = OperationRHS<R> &
+  OperationOutput<O>;
+
+// Scalar utility types
+type CommonScalarAdditiveCombinationResult =
+  | AdditiveCombinationResult<typeof DATATYPE.MATRIX2, typeof DATATYPE.MATRIX2>
+  | AdditiveCombinationResult<typeof DATATYPE.MATRIX3, typeof DATATYPE.MATRIX3>
+  | AdditiveCombinationResult<typeof DATATYPE.MATRIX4, typeof DATATYPE.MATRIX4>
+  | AdditiveCombinationResult<
+      typeof DATATYPE.MATRIX2x3,
+      typeof DATATYPE.MATRIX2x3
+    >
+  | AdditiveCombinationResult<
+      typeof DATATYPE.MATRIX2x4,
+      typeof DATATYPE.MATRIX2x4
+    >
+  | AdditiveCombinationResult<
+      typeof DATATYPE.MATRIX3x2,
+      typeof DATATYPE.MATRIX3x2
+    >
+  | AdditiveCombinationResult<
+      typeof DATATYPE.MATRIX3x4,
+      typeof DATATYPE.MATRIX3x4
+    >
+  | AdditiveCombinationResult<
+      typeof DATATYPE.MATRIX4x2,
+      typeof DATATYPE.MATRIX4x2
+    >
+  | AdditiveCombinationResult<
+      typeof DATATYPE.MATRIX4x3,
+      typeof DATATYPE.MATRIX4x3
+    >;
+
+// Float vector utility types
+type VecAdditiveCombinationResult<O extends FloatVecDatatype> =
+  | AdditiveCombinationResult<typeof DATATYPE.FLOAT, O>
+  | AdditiveCombinationResult<typeof DATATYPE.INT, O>
+  | AdditiveCombinationResult<typeof DATATYPE.UINT, O>
+  | AdditiveCombinationResult<O, O>;
+
+// Int vector utility types
+type IntVecAdditiveResult<U, F> = {
+  uint: U;
+  float: F;
+};
+
+type IntVecAdditiveResultTypes<T extends IntVecDatatype> = {
+  [INT_VEC_DATATYPE.INT_VEC2]: IntVecAdditiveResult<
+    typeof DATATYPE.UINT_VEC2,
+    typeof DATATYPE.VEC2
+  >;
+  [INT_VEC_DATATYPE.INT_VEC3]: IntVecAdditiveResult<
+    typeof DATATYPE.UINT_VEC3,
+    typeof DATATYPE.VEC3
+  >;
+  [INT_VEC_DATATYPE.INT_VEC4]: IntVecAdditiveResult<
+    typeof DATATYPE.UINT_VEC4,
+    typeof DATATYPE.VEC4
+  >;
+}[T];
+
+type IntVecAdditiveCombinationResult<O extends IntVecDatatype> =
+  | AdditiveCombinationResult<typeof DATATYPE.FLOAT, O>
+  | AdditiveCombinationResult<
+      typeof DATATYPE.INT,
+      IntVecAdditiveResultTypes<O>["float"]
+    >
+  | AdditiveCombinationResult<
+      typeof DATATYPE.UINT,
+      IntVecAdditiveResultTypes<O>["uint"]
+    >
+  | AdditiveCombinationResult<O, O>;
+
+// Unsigned vector utility types
+
+type UintVecFloatType<T extends UintVecDatatype> = {
+  [UINT_VEC_DATATYPE.UINT_VEC2]: typeof DATATYPE.VEC2;
+  [UINT_VEC_DATATYPE.UINT_VEC3]: typeof DATATYPE.VEC3;
+  [UINT_VEC_DATATYPE.UINT_VEC4]: typeof DATATYPE.VEC4;
+}[T];
+
+type UintVecAdditiveCombinationResult<O extends UintVecDatatype> =
+  | AdditiveCombinationResult<typeof DATATYPE.FLOAT, O>
+  | AdditiveCombinationResult<typeof DATATYPE.INT, O>
+  | AdditiveCombinationResult<typeof DATATYPE.UINT, UintVecFloatType<O>>
+  | AdditiveCombinationResult<O, O>;
+
+// Matrix utility types
+type MatrixAdditiveCombinationResult<O extends MatrixDatatype> =
+  | AdditiveCombinationResult<typeof DATATYPE.FLOAT, O>
+  | AdditiveCombinationResult<typeof DATATYPE.INT, O>
+  | AdditiveCombinationResult<typeof DATATYPE.UINT, O>
+  | AdditiveCombinationResult<O, O>;
+
+export type AdditiveDatatype =
+  | Exclude<ScalarDataType, typeof SCALAR_DATATYPE.BOOL>
+  | FloatVecDatatype
+  | IntVecDatatype
+  | UintVecDatatype
+  | MatrixDatatype;
+
+export type AdditiveCombination<T extends AdditiveDatatype> = {
+  // Scalar
+  [DATATYPE.FLOAT]:
+    | AdditiveCombinationResult<typeof DATATYPE.FLOAT, typeof DATATYPE.FLOAT>
+    | AdditiveCombinationResult<typeof DATATYPE.INT, typeof DATATYPE.FLOAT>
+    | AdditiveCombinationResult<typeof DATATYPE.UINT, typeof DATATYPE.FLOAT>
+    | AdditiveCombinationResult<typeof DATATYPE.VEC2, typeof DATATYPE.VEC2>
+    | AdditiveCombinationResult<typeof DATATYPE.VEC3, typeof DATATYPE.VEC3>
+    | AdditiveCombinationResult<typeof DATATYPE.VEC4, typeof DATATYPE.VEC4>
+    | AdditiveCombinationResult<typeof DATATYPE.INT_VEC2, typeof DATATYPE.VEC2>
+    | AdditiveCombinationResult<typeof DATATYPE.INT_VEC3, typeof DATATYPE.VEC3>
+    | AdditiveCombinationResult<typeof DATATYPE.INT_VEC4, typeof DATATYPE.VEC4>
+    | AdditiveCombinationResult<typeof DATATYPE.UINT_VEC2, typeof DATATYPE.VEC2>
+    | AdditiveCombinationResult<typeof DATATYPE.UINT_VEC3, typeof DATATYPE.VEC3>
+    | AdditiveCombinationResult<typeof DATATYPE.UINT_VEC4, typeof DATATYPE.VEC4>
+    | CommonScalarAdditiveCombinationResult;
+
+  [DATATYPE.INT]:
+    | AdditiveCombinationResult<typeof DATATYPE.FLOAT, typeof DATATYPE.FLOAT>
+    | AdditiveCombinationResult<typeof DATATYPE.INT, typeof DATATYPE.INT>
+    | AdditiveCombinationResult<typeof DATATYPE.UINT, typeof DATATYPE.UINT>
+    | AdditiveCombinationResult<typeof DATATYPE.VEC2, typeof DATATYPE.VEC2>
+    | AdditiveCombinationResult<typeof DATATYPE.VEC3, typeof DATATYPE.VEC3>
+    | AdditiveCombinationResult<typeof DATATYPE.VEC4, typeof DATATYPE.VEC4>
+    | AdditiveCombinationResult<
+        typeof DATATYPE.INT_VEC2,
+        typeof DATATYPE.INT_VEC2
+      >
+    | AdditiveCombinationResult<
+        typeof DATATYPE.INT_VEC3,
+        typeof DATATYPE.INT_VEC3
+      >
+    | AdditiveCombinationResult<
+        typeof DATATYPE.INT_VEC4,
+        typeof DATATYPE.INT_VEC4
+      >
+    | AdditiveCombinationResult<
+        typeof DATATYPE.UINT_VEC2,
+        typeof DATATYPE.UINT_VEC2
+      >
+    | AdditiveCombinationResult<
+        typeof DATATYPE.UINT_VEC3,
+        typeof DATATYPE.UINT_VEC3
+      >
+    | AdditiveCombinationResult<
+        typeof DATATYPE.UINT_VEC4,
+        typeof DATATYPE.UINT_VEC4
+      >
+    | CommonScalarAdditiveCombinationResult;
+
+  [DATATYPE.UINT]:
+    | AdditiveCombinationResult<typeof DATATYPE.FLOAT, typeof DATATYPE.FLOAT>
+    | AdditiveCombinationResult<typeof DATATYPE.INT, typeof DATATYPE.UINT>
+    | AdditiveCombinationResult<typeof DATATYPE.UINT, typeof DATATYPE.UINT>
+    | AdditiveCombinationResult<typeof DATATYPE.VEC2, typeof DATATYPE.VEC2>
+    | AdditiveCombinationResult<typeof DATATYPE.VEC3, typeof DATATYPE.VEC3>
+    | AdditiveCombinationResult<typeof DATATYPE.VEC4, typeof DATATYPE.VEC4>
+    | AdditiveCombinationResult<
+        typeof DATATYPE.INT_VEC2,
+        typeof DATATYPE.UINT_VEC2
+      >
+    | AdditiveCombinationResult<
+        typeof DATATYPE.INT_VEC3,
+        typeof DATATYPE.UINT_VEC3
+      >
+    | AdditiveCombinationResult<
+        typeof DATATYPE.INT_VEC4,
+        typeof DATATYPE.UINT_VEC4
+      >
+    | AdditiveCombinationResult<
+        typeof DATATYPE.UINT_VEC2,
+        typeof DATATYPE.UINT_VEC2
+      >
+    | AdditiveCombinationResult<
+        typeof DATATYPE.UINT_VEC3,
+        typeof DATATYPE.UINT_VEC3
+      >
+    | AdditiveCombinationResult<
+        typeof DATATYPE.UINT_VEC4,
+        typeof DATATYPE.UINT_VEC4
+      >
+    | CommonScalarAdditiveCombinationResult;
+
+  // Float vectors
+  [DATATYPE.VEC2]: VecAdditiveCombinationResult<typeof DATATYPE.VEC2>;
+  [DATATYPE.VEC3]: VecAdditiveCombinationResult<typeof DATATYPE.VEC3>;
+  [DATATYPE.VEC4]: VecAdditiveCombinationResult<typeof DATATYPE.VEC4>;
+
+  // Signed vectors
+  [DATATYPE.INT_VEC2]: IntVecAdditiveCombinationResult<
+    typeof DATATYPE.INT_VEC2
+  >;
+  [DATATYPE.INT_VEC3]: IntVecAdditiveCombinationResult<
+    typeof DATATYPE.INT_VEC3
+  >;
+  [DATATYPE.INT_VEC4]: IntVecAdditiveCombinationResult<
+    typeof DATATYPE.INT_VEC4
+  >;
+
+  // Unsigned vectors
+  [DATATYPE.UINT_VEC2]: UintVecAdditiveCombinationResult<
+    typeof DATATYPE.UINT_VEC2
+  >;
+  [DATATYPE.UINT_VEC3]: UintVecAdditiveCombinationResult<
+    typeof DATATYPE.UINT_VEC3
+  >;
+  [DATATYPE.UINT_VEC4]: UintVecAdditiveCombinationResult<
+    typeof DATATYPE.UINT_VEC4
+  >;
+
+  // Matrices
+  [DATATYPE.MATRIX2]: MatrixAdditiveCombinationResult<typeof DATATYPE.MATRIX2>;
+  [DATATYPE.MATRIX3]: MatrixAdditiveCombinationResult<typeof DATATYPE.MATRIX3>;
+  [DATATYPE.MATRIX4]: MatrixAdditiveCombinationResult<typeof DATATYPE.MATRIX4>;
+
+  [DATATYPE.MATRIX2x3]: MatrixAdditiveCombinationResult<
+    typeof DATATYPE.MATRIX2x3
+  >;
+  [DATATYPE.MATRIX2x4]: MatrixAdditiveCombinationResult<
+    typeof DATATYPE.MATRIX2x4
+  >;
+  [DATATYPE.MATRIX3x2]: MatrixAdditiveCombinationResult<
+    typeof DATATYPE.MATRIX3x2
+  >;
+  [DATATYPE.MATRIX3x4]: MatrixAdditiveCombinationResult<
+    typeof DATATYPE.MATRIX3x4
+  >;
+  [DATATYPE.MATRIX4x2]: MatrixAdditiveCombinationResult<
+    typeof DATATYPE.MATRIX4x2
+  >;
+  [DATATYPE.MATRIX4x3]: MatrixAdditiveCombinationResult<
+    typeof DATATYPE.MATRIX4x3
+  >;
+}[T];
