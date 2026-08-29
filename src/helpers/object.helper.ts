e35547960bbbd29e28021e54c9e4f919ea683d31
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-29 14:10:02 +0330

    refactor: Improve object helper functions by adding values function and enhancing valuesInclude for better type handling

--- src/helpers/object.helper.ts
+++ src/helpers/object.helper.ts
@@ -1,5 +1,9 @@
 export type ObjectValues<T extends object> = T[keyof T];
 
+export function values<T extends object>(target: T) {
+  return Object.values(target) as ObjectValues<T>[];
+}
+
 export function omit<T extends object, K extends keyof T>(
   target: T,
   keys: K[],
@@ -11,4 +15,13 @@ export function omit<T extends object, K extends keyof T>(
   }
 
   return result;
+}
+
+export function valuesInclude<T extends object>(
+  target: T,
+  key: unknown,
+): key is ObjectValues<T> {
+  const objectValues = values(target);
+
+  return objectValues.includes(key as ObjectValues<T>);
 }
