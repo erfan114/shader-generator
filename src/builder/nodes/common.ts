6e19b4675d3fd4b109d6b99d3920342af5027a9f
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-31 00:00:33 +0330

    refactor: Remove unused comment block in common.ts

--- src/builder/nodes/common.ts
+++ src/builder/nodes/common.ts
@@ -2,16 +2,6 @@ import type { Datatype } from "../../types.js";
 
 export type Node<T extends PropertyKey, D> = { kind: T } & D;
 
-/* 
-
-How we are gonna assign everything together with type safety
-
-
-
-
-
-*/
-
 export type IODeclarationOptions = {
   name: string;
   type: Datatype;
