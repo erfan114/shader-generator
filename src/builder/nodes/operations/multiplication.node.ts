29a886c23f30a73a24814b844bf9fde5033475ec
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-31 01:21:39 +0330

    refactor: Enforce MultiplicativeDatatype constraints in MultiplicationNode for improved type safety

--- src/builder/nodes/operations/multiplication.node.ts
+++ src/builder/nodes/operations/multiplication.node.ts
@@ -1,3 +1,7 @@
 import type { OperationNode } from "./common.js";
+import type { MultiplicativeDatatype } from "./types/multiplicative.type.js";
 
-export type MultiplicationNode<L, R> = OperationNode<"multiplication", L, R>;
+export type MultiplicationNode<
+  L extends MultiplicativeDatatype,
+  R extends MultiplicativeDatatype,
+> = OperationNode<"multiplication", L, R>;
