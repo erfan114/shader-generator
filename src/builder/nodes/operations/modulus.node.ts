73fd5797257e31f26d1dd7ee5a35dbb3eed54312
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-31 05:36:01 +0330

    refactor: Implement modulo function in ModulusNode for performing modulus operations

--- src/builder/nodes/operations/modulus.node.ts
+++ src/builder/nodes/operations/modulus.node.ts
@@ -1,3 +1,11 @@
 import type { OperationNode } from "./common.js";
 
 export type ModulusNode<L, R> = OperationNode<"modulus", L, R>;
+
+export function modulo<L, R>(left: L, right: R): ModulusNode<L, R> {
+  return {
+    kind: "modulus",
+    left,
+    right,
+  };
+}
