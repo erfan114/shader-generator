9e337737e1c4588853845746278627f299328405
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-31 05:34:03 +0330

    refactor: Implement divide function in DivisionNode for performing division operations

--- src/builder/nodes/operations/division.node.ts
+++ src/builder/nodes/operations/division.node.ts
@@ -5,3 +5,14 @@ export type DivisionNode<
   L extends MultiplicativeDatatype,
   R extends MultiplicativeDatatype,
 > = OperationNode<"division", L, R>;
+
+export function divide<
+  L extends MultiplicativeDatatype,
+  R extends MultiplicativeDatatype,
+>(left: L, right: R): DivisionNode<L, R> {
+  return {
+    kind: "division",
+    left,
+    right,
+  };
+}
