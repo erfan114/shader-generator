eb234a47e4fe05fec59a9fa50ed1fc48374f83d2
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-31 05:22:07 +0330

    refactor: Update OutputNode and OutputNodeModel to support generic type parameters

--- src/builder/nodes/output.node.ts
+++ src/builder/nodes/output.node.ts
@@ -2,9 +2,8 @@ import type { Datatype } from "../../types.js";
 import { NodeModel } from "../node.js";
 import type { IONode } from "./common.js";
 
-export type OutputNode<Type extends Datatype = Datatype> = IONode<
-  "output",
-  Type
->;
+export type OutputNode<Type extends Datatype> = IONode<"output", Type>;
 
-export class OutputNodeModel extends NodeModel<OutputNode> {}
+export class OutputNodeModel<Type extends Datatype> extends NodeModel<
+  OutputNode<Type>
+> {}
