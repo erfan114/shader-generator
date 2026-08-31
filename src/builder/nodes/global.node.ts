4b31de81b15f76b57cde4e4d3d13742e9c7e8c96
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-31 09:43:26 +0330

    refactor: Implement GlobalNodeModel with methods for define, uniform, input, output, and function creation

--- src/builder/nodes/global.node.ts
+++ src/builder/nodes/global.node.ts
@@ -1,5 +1,12 @@
+import { NotImplementedError } from "../../errors.js";
+import type { Datatype } from "../../types.js";
 import { type BuilderNode, NodeModel, type OwnableNode } from "../node.js";
-import type { DefineNode } from "./define.node.js";
+import type { IONodeOptions } from "./common.js";
+import type { DefineNode, DefineNodeModel } from "./define.node.js";
+import type { FunctionNodeModel } from "./function.node.js";
+import type { InputNodeModel } from "./input.node.js";
+import type { OutputNodeModel } from "./output.node.js";
+import type { UniformNodeModel, UniformNodeOptions } from "./uniform.node.js";
 
 export type GlobalNode = BuilderNode<
   "global",
@@ -13,4 +20,30 @@ export type GlobalOwnedNode<
   Data = unknown,
 > = OwnableNode<Kind, GlobalNode, Data>;
 
-export class GlobalNodeModel extends NodeModel<GlobalNode> {}
+export class GlobalNodeModel extends NodeModel<GlobalNode> {
+  public createDefine(): DefineNodeModel {
+    throw new NotImplementedError();
+  }
+
+  public createUniform<Type extends Datatype>(
+    options: UniformNodeOptions<Type>,
+  ): UniformNodeModel<Type> {
+    throw new NotImplementedError();
+  }
+
+  public createInput<Type extends Datatype>(
+    options: IONodeOptions<Type>,
+  ): InputNodeModel<Type> {
+    throw new NotImplementedError();
+  }
+
+  public createOutput<Type extends Datatype>(
+    options: IONodeOptions<Type>,
+  ): OutputNodeModel<Type> {
+    throw new NotImplementedError();
+  }
+
+  public createFunction(): FunctionNodeModel {
+    throw new NotImplementedError();
+  }
+}
