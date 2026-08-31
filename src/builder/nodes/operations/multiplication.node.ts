b33cf48d423d7914911adcbb581c665d0402a507
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-08-31 05:34:27 +0330

    refactor: Implement multiply function in MultiplicationNode for performing multiplication operations

--- src/builder/nodes/operations/multiplication.node.ts
+++ src/builder/nodes/operations/multiplication.node.ts
@@ -5,3 +5,14 @@ export type MultiplicationNode<
   L extends MultiplicativeDatatype,
   R extends MultiplicativeDatatype,
 > = OperationNode<"multiplication", L, R>;
+
+export function multiply<
+  L extends MultiplicativeDatatype,
+  R extends MultiplicativeDatatype,
+>(left: L, right: R): MultiplicationNode<L, R> {
+  return {
+    kind: "multiplication",
+    left,
+    right,
+  };
+}
