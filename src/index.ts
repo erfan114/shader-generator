8f2a95d017efa48b5be6707bb65bf62c50e956f6
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-01 17:43:40 +0330

    refactor: Add exports for builder node operations and uniform node

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export type { BuildTarget } from "./compiler/index.js";
 export { Builder } from "./builder/index.js";
+export { uniform } from "./builder/nodes/uniform.node.js";
+export { add } from "./builder/nodes/operations/addition.node.js";
+export { subtract } from "./builder/nodes/operations/subtraction.node.js";
+export { multiply } from "./builder/nodes/operations/multiplication.node.js";
+export { divide } from "./builder/nodes/operations/division.node.js";
+export { modulo } from "./builder/nodes/operations/modulus.node.js";
