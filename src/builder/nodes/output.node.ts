7a08f895fcaa70ce11d27d5c665d10d5ff4fe03a
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-02 12:01:34 +0330

    feat: Enhance OutputNode implementation with dynamic kind and builderNode integration

--- src/builder/nodes/output.node.ts
+++ src/builder/nodes/output.node.ts
@@ -1,4 +1,39 @@
 import type { Datatype } from "../../types.js";
-import type { IONode } from "./common.js";
+import { builderNode } from "../node.js";
+import type {
+  IONode,
+  IONodeData,
+  IONodeMethods,
+  IONodeOptions,
+} from "./common.js";
 
-export type OutputNode<Type extends Datatype> = IONode<"output", Type>;
+const OUTPUT_KIND = "output";
+
+export type OutputNode<Type extends Datatype> = IONode<
+  typeof OUTPUT_KIND,
+  Type
+>;
+
+export function output<Type extends Datatype>(
+  options: IONodeOptions<Type>,
+): OutputNode<Type> {
+  const create = (data: IONodeData<Type>): OutputNode<Type> => {
+    return builderNode<
+      typeof OUTPUT_KIND,
+      IONodeOptions<Type>,
+      IONodeMethods<typeof OUTPUT_KIND, Type>
+    >({
+      kind: OUTPUT_KIND,
+      data,
+
+      as(alias) {
+        return create({
+          ...data,
+          name: alias,
+        });
+      },
+    });
+  };
+
+  return create(options);
+}
