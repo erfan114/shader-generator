0ed342bb9471d7cf051b6524b44ff38b154b5030
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-02 11:05:12 +0330

    test: Add unit test for input node aliasing functionality

---
+++ tests/builder/input.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest";
+
+import { input } from "../../src/builder/nodes/input.node.js";
+import { DATATYPE } from "../../src/types.js";
+
+describe("Input", () => {
+  it("Should alias its name", () => {
+    const ALIAS_NAME = "input1";
+
+    const targetInput = input({ type: DATATYPE.FLOAT });
+
+    expect(targetInput.data.name).toBeUndefined();
+
+    const aliasedInput = targetInput.as(ALIAS_NAME);
+
+    expect(aliasedInput.data.name).toBe(ALIAS_NAME);
+  });
+});
