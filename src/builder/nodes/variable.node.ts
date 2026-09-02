425581b0cad02b074e68314ae1f2336cf44f05f1
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-02 17:06:18 +0330

    refactor: Enhance VariableNode type definitions and structure for clarity

--- src/builder/nodes/variable.node.ts
+++ src/builder/nodes/variable.node.ts
@@ -1,21 +1,58 @@
-import type { BuilderNode } from "../node.js";
-import type {
-  DatatypeValueType,
-  ValueDataType,
-  ValueNode,
-} from "./value.node.js";
+import { type BuilderNode, builderNode } from "../node.js";
+import type { DatatypeValueType, ValueDataType } from "./value.node.js";
 
-export type VariableObjectProps<T extends ValueDataType> = {
+const VARIABLE_KIND = "variable";
+
+export type VariableNodeOptions<Type extends ValueDataType> = {
+  type: Type;
+};
+
+export type VariableNodeStates<Type extends ValueDataType> = Partial<{
   name: string;
-  type: T;
-  value: DatatypeValueType<T> | null;
+  value: DatatypeValueType<Type>;
+}>;
+
+export type VariableNodeData<Type extends ValueDataType> =
+  VariableNodeOptions<Type> & VariableNodeStates<Type>;
+
+export type VariableNodeMethods<Type extends ValueDataType> = {
+  assign(value: DatatypeValueType<Type>): VariableNode<Type>;
+  as(alias: string): VariableNode<Type>;
 };
 
 export type VariableNode<Type extends ValueDataType = ValueDataType> =
   BuilderNode<
-    "variable",
-    {
-      name: string;
-      value: ValueNode<Type>;
-    }
+    typeof VARIABLE_KIND,
+    VariableNodeData<Type>,
+    VariableNodeMethods<Type>
   >;
+
+export function variable<Type extends ValueDataType>(
+  options: VariableNodeOptions<Type>,
+): VariableNode<Type> {
+  const create = (data: VariableNodeData<Type>): VariableNode<Type> => {
+    return builderNode<
+      typeof VARIABLE_KIND,
+      VariableNodeOptions<Type>,
+      VariableNodeMethods<Type>
+    >({
+      kind: VARIABLE_KIND,
+      data,
+
+      as(alias) {
+        return create({
+          ...data,
+          name: alias,
+        });
+      },
+      assign(value) {
+        return create({
+          ...data,
+          value,
+        });
+      },
+    });
+  };
+
+  return create(options);
+}
