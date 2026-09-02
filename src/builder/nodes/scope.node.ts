6dabd3fa2ef07cdb5b012847c3eeba57ba02ed89
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-02 14:09:55 +0330

    refactor: Update ScopeBody type definition to clarify generator return type

--- src/builder/nodes/scope.node.ts
+++ src/builder/nodes/scope.node.ts
@@ -2,10 +2,9 @@ import { type BuilderNode, builderNode } from "../node.js";
 import type { ValueDataType } from "./value.node.js";
 
 // * SCOPE BODY
-export type ScopeBody<Returns extends ValueDataType | null> = () => Generator<
-  unknown,
-  Returns
->;
+export type ScopeBody<Returns extends ValueDataType | null> =
+  // TODO: Generator shouldn't yield unknown, fix it
+  () => Generator<unknown, Returns>;
 
 // * SCOPE NODE
 const SCOPE_KIND = "scope";
