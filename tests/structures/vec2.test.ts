44e6f92fa35ff03b928a37c7a6835494f72b48e0
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-05 08:09:37 +0330

    test: Remove duplicate test for vec2 creation with a single scalar value

--- tests/structures/vec2.test.ts
+++ tests/structures/vec2.test.ts
@@ -3,17 +3,17 @@ import { describe, expect, it } from "vitest";
 import { isVec2, vec2 } from "@/structures/vec2.structure.js";
 
 describe("Vec2", () => {
+  it("should create within one scalar", () => {
+    const v = 20;
+
+    expect(vec2(v)).toStrictEqual([v, v]);
+  });
+
   it("should create within two args", () => {
     const x = 10;
     const y = 20;
 
     expect(vec2(x, y)).toStrictEqual([x, y]);
-  });
-
-  it("should create within one arg", () => {
-    const v = 20;
-
-    expect(vec2(v)).toStrictEqual([v, v]);
   });
 
   it("should pass 'isVec2'", () => {
