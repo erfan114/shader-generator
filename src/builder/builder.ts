8808eee58c9325c239b26988626f81c8e6e8658b
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-05 10:19:46 +0330

    refactor: Update Builder class to process nodes from generator and validate final return as MainNode

--- src/builder/builder.ts
+++ src/builder/builder.ts
@@ -2,15 +2,40 @@ import type { BuildTarget } from "@/compiler/index.js";
 import { NotImplementedError } from "@/errors.js";
 
 import type { BuilderNode } from "./node.js";
+import { type MainNode, isMainNode } from "./nodes/main.node.js";
 
-export type BuilderGenerator = () => Generator<BuilderNode>;
+export type BuilderGenerator = () => Generator<
+  BuilderNode,
+  MainNode,
+  BuilderNode
+>;
+
+type BuilderNodes = BuilderNode[];
 
 export class Builder {
   public static from_generator(generator: BuilderGenerator): Builder {
-    return new Builder(generator);
+    const nodes: BuilderNodes = [];
+    const instance = generator();
+
+    let current = instance.next();
+
+    while (!current.done) {
+      nodes.push(current.value);
+      current = instance.next(current.value);
+    }
+
+    if (!isMainNode(current.value)) {
+      throw new Error(
+        "Invalid builder generator: its final return value must be a 'MainNode'",
+      );
+    }
+
+    nodes.push(current.value);
+
+    return new Builder(nodes);
   }
 
-  private constructor(protected readonly generator: BuilderGenerator) {}
+  private constructor(protected readonly nodes: BuilderNodes) {}
 
   /**
    * Builds the shader for the specified target.
