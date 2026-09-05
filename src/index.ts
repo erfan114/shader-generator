01c6ac552bdf80033b5e4e439c47e41176cdba64
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-05 08:24:35 +0330

    refactor: Reorder exports in index.ts for improved organization

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-export type { BuildTarget } from "@/compiler/index.js";
 export { Builder } from "@/builder/index.js";
+
+export type { BuildTarget } from "@/compiler/index.js";
+
+export type { Vec2 } from "@/structures/vec2.structure.js";
+export type { Vec3 } from "@/structures/vec3.structure.js";
+export type { Vec4 } from "@/structures/vec4.structure.js";
+
+export { DATATYPE } from "@/types.js";
+
 export { uniform } from "@/builder/nodes/uniform.node.js";
 export { add } from "@/builder/nodes/operations/addition.node.js";
 export { subtract } from "@/builder/nodes/operations/subtraction.node.js";
 export { multiply } from "@/builder/nodes/operations/multiplication.node.js";
 export { divide } from "@/builder/nodes/operations/division.node.js";
 export { modulo } from "@/builder/nodes/operations/modulus.node.js";
-export { DATATYPE } from "@/types.js";
+export { vec2 } from "@/structures/vec2.structure.js";
+export { vec3 } from "@/structures/vec3.structure.js";
+export { vec4 } from "@/structures/vec4.structure.js";
