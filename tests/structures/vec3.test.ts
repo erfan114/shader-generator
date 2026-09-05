beafb4be11f3f81afc2accc70492b85586138161
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-05 07:50:30 +0330

    test: Add isVec3 validation tests to vec3 test suite

--- tests/structures/vec3.test.ts
+++ tests/structures/vec3.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 
 import { vec2 } from "@/structures/vec2.structure.js";
-import { vec3 } from "@/structures/vec3.structure.js";
+import { isVec3, vec3 } from "@/structures/vec3.structure.js";
 
 describe("Vec3", () => {
   it("should create within three args", () => {
@@ -18,5 +18,16 @@ describe("Vec3", () => {
 
     expect(vec3(x, y)).toStrictEqual([x, ...y]);
     expect(vec3(y, x)).toStrictEqual([...y, x]);
+  });
+
+  it("should pass 'isVec3'", () => {
+    const myVec = vec3(10);
+    const invalidEntries = ["test", 10, null, undefined, vec2(10)];
+
+    expect(isVec3(myVec)).toBe(true);
+
+    for (const invalidEntry of invalidEntries) {
+      expect(isVec3(invalidEntry)).toBe(false);
+    }
   });
 });
