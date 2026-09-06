83a4c3f9dc3c97400bf9a0755caf783133adb833
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-06 07:18:33 +0330

    refactor: Reorganize exports in index.ts for better structure and readability

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
+// Builder
 export { Builder } from "@/builder/index.js";
 
+// Compiler
 export type { BuildTarget } from "@/compiler/index.js";
 
-export type { Vec2 } from "@/structures/vec2.structure.js";
-export type { Vec3 } from "@/structures/vec3.structure.js";
-export type { Vec4 } from "@/structures/vec4.structure.js";
-
+// Data types
 export { DATATYPE } from "@/types.js";
 
-export { uniform } from "@/builder/nodes/uniform.node.js";
+// Nodes
+export { fn } from "@/builder/nodes/function.node.js";
 export { add } from "@/builder/nodes/operations/addition.node.js";
-export { subtract } from "@/builder/nodes/operations/subtraction.node.js";
-export { multiply } from "@/builder/nodes/operations/multiplication.node.js";
 export { divide } from "@/builder/nodes/operations/division.node.js";
 export { modulo } from "@/builder/nodes/operations/modulus.node.js";
+export { multiply } from "@/builder/nodes/operations/multiplication.node.js";
+export { subtract } from "@/builder/nodes/operations/subtraction.node.js";
+export { uniform } from "@/builder/nodes/uniform.node.js";
+
+// Structures
+export type { Vec2 } from "@/structures/vec2.structure.js";
 export { vec2 } from "@/structures/vec2.structure.js";
+export type { Vec3 } from "@/structures/vec3.structure.js";
 export { vec3 } from "@/structures/vec3.structure.js";
+export type { Vec4 } from "@/structures/vec4.structure.js";
 export { vec4 } from "@/structures/vec4.structure.js";
