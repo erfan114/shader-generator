dc7d566f18cfa64443ae64e36ec23ad34485a1cf
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-06 07:29:57 +0330

    refactor: Add missing node exports for better accessibility in index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,20 @@ export { DATATYPE } from "@/types.js";
 
 // Nodes
 export { fn } from "@/builder/nodes/function.node.js";
+export { input } from "@/builder/nodes/input.node.js";
+export { main } from "@/builder/nodes/main.node.js";
+export { output } from "@/builder/nodes/output.node.js";
+export { scope } from "@/builder/nodes/scope.node.js";
+export { uniform } from "@/builder/nodes/uniform.node.js";
+export { value } from "@/builder/nodes/value.node.js";
+export { variable } from "@/builder/nodes/variable.node.js";
+
+// Operational nodes
 export { add } from "@/builder/nodes/operations/addition.node.js";
 export { divide } from "@/builder/nodes/operations/division.node.js";
 export { modulo } from "@/builder/nodes/operations/modulus.node.js";
 export { multiply } from "@/builder/nodes/operations/multiplication.node.js";
 export { subtract } from "@/builder/nodes/operations/subtraction.node.js";
-export { uniform } from "@/builder/nodes/uniform.node.js";
 
 // Structures
 export type { Vec2 } from "@/structures/vec2.structure.js";
