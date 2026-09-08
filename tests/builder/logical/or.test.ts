e231946ada71f4ea006bdfeee651a7c52566a54e
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-08 15:06:27 +0330

    feat: Update 'or' test to use constants for operator and kind

--- tests/builder/logical/or.test.ts
+++ tests/builder/logical/or.test.ts
@@ -1,11 +1,12 @@
 import { eq } from "@/builder/nodes/comparison/eq.node.js";
-import { or } from "@/builder/nodes/logical/or.node.js";
+import { LOGICAL_KIND } from "@/builder/nodes/logical/common.js";
+import { or, OR_OPERATOR } from "@/builder/nodes/logical/or.node.js";
 import { value } from "@/builder/nodes/value.node.js";
 import { DATATYPE } from "@/types.js";
 import { describe, expect, it } from "vitest";
 
 describe("or", () => {
-  it("should create a logical node with operator ||", () => {
+  it(`should create a logical node with operator ${OR_OPERATOR}`, () => {
     const result = or(
       eq(
         value({ type: DATATYPE.FLOAT, data: 1 }),
@@ -17,7 +18,7 @@ describe("or", () => {
       ),
     );
 
-    expect(result.data.operator).toBe("||");
-    expect(result.kind).toBe("logical");
+    expect(result.data.operator).toBe(OR_OPERATOR);
+    expect(result.kind).toBe(LOGICAL_KIND);
   });
 });
