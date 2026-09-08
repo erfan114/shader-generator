bed1bb2c5c17c658100be0c3a9c8ac9a16701d81
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-08 14:56:10 +0330

    feat: Update logical nodes section in index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,10 @@ export { modulo } from "@/builder/nodes/operations/modulus.node.js";
 export { multiply } from "@/builder/nodes/operations/multiplication.node.js";
 export { subtract } from "@/builder/nodes/operations/subtraction.node.js";
 
-// Boolean condition nodes
+// Logical nodes
+export { and } from "@/builder/nodes/logical/and.node.js";
+export { not } from "@/builder/nodes/logical/not.node.js";
+export { or } from "@/builder/nodes/logical/or.node.js";
 
 // Structures
 export type { Vec2 } from "@/structures/vec2.structure.js";
