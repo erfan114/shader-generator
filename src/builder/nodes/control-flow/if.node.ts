0872fb94d5f377e70c95f69a2f9362db59fa32af
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-08 16:33:30 +0330

    feat: Refactor if_ function to accept options and return BuilderNode

--- src/builder/nodes/control-flow/if.node.ts
+++ src/builder/nodes/control-flow/if.node.ts
@@ -1,9 +1,19 @@
-import { NotImplementedError } from "@/errors.js";
 import type { BooleanExpression } from "../logical/common.js";
 import type { ControlFlowBody } from "./common.js";
+import { builderNode, type BuilderNode } from "@/builder/node.js";
 
 export const IF_KIND = "if";
 
-export function if_(expression: BooleanExpression, body: ControlFlowBody) {
-  throw new NotImplementedError();
+export type IfNodeOptions = {
+  expression: BooleanExpression;
+  body: ControlFlowBody;
+};
+
+export type IfNode = BuilderNode<typeof IF_KIND, IfNodeOptions>;
+
+export function if_(options: IfNodeOptions): IfNode {
+  return builderNode({
+    kind: IF_KIND,
+    data: options,
+  });
 }
