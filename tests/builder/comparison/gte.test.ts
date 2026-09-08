71ebfc1b462b26bed020c8f65f359c49814e6f52
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-08 15:08:26 +0330

    feat: Update gte test to use constants for operator and kind

--- tests/builder/comparison/gte.test.ts
+++ tests/builder/comparison/gte.test.ts
@@ -1,16 +1,20 @@
-import { gte } from "@/builder/nodes/comparison/gte.node.js";
+import { COMPARISON_KIND } from "@/builder/nodes/comparison/common.js";
+import {
+  GREATER_THAN_EQUAL_OPERATOR,
+  gte,
+} from "@/builder/nodes/comparison/gte.node.js";
 import { value } from "@/builder/nodes/value.node.js";
 import { DATATYPE } from "@/types.js";
 import { describe, expect, it } from "vitest";
 
 describe("gte", () => {
-  it("should create a comparison node with operator >=", () => {
+  it(`should create a comparison node with operator ${GREATER_THAN_EQUAL_OPERATOR}`, () => {
     const result = gte(
       value({ type: DATATYPE.FLOAT, data: 1 }),
       value({ type: DATATYPE.FLOAT, data: 2 }),
     );
 
-    expect(result.data.operator).toBe(">=");
-    expect(result.kind).toBe("comparison");
+    expect(result.data.operator).toBe(GREATER_THAN_EQUAL_OPERATOR);
+    expect(result.kind).toBe(COMPARISON_KIND);
   });
 });
