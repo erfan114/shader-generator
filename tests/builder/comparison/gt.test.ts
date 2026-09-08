d6cc58d1514e6ed2104883ed8f40b7251d39e4a9
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-08 15:07:59 +0330

    feat: Update gt test to use constants for operator and kind

--- tests/builder/comparison/gt.test.ts
+++ tests/builder/comparison/gt.test.ts
@@ -1,16 +1,20 @@
-import { gt } from "@/builder/nodes/comparison/gt.node.js";
+import { COMPARISON_KIND } from "@/builder/nodes/comparison/common.js";
+import {
+  GREATER_THAN_OPERATOR,
+  gt,
+} from "@/builder/nodes/comparison/gt.node.js";
 import { value } from "@/builder/nodes/value.node.js";
 import { DATATYPE } from "@/types.js";
 import { describe, expect, it } from "vitest";
 
 describe("gt", () => {
-  it("should create a comparison node with operator >", () => {
+  it(`should create a comparison node with operator ${GREATER_THAN_OPERATOR}`, () => {
     const result = gt(
       value({ type: DATATYPE.FLOAT, data: 1 }),
       value({ type: DATATYPE.FLOAT, data: 2 }),
     );
 
-    expect(result.data.operator).toBe(">");
-    expect(result.kind).toBe("comparison");
+    expect(result.data.operator).toBe(GREATER_THAN_OPERATOR);
+    expect(result.kind).toBe(COMPARISON_KIND);
   });
 });
