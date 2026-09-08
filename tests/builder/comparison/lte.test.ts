c92b159b862503f7c23ddfc8ba9b29ac84b77767
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-08 15:09:26 +0330

    feat: Update lte test to use constants for operator and kind

--- tests/builder/comparison/lte.test.ts
+++ tests/builder/comparison/lte.test.ts
@@ -1,16 +1,20 @@
-import { lte } from "@/builder/nodes/comparison/lte.node.js";
+import { COMPARISON_KIND } from "@/builder/nodes/comparison/common.js";
+import {
+  LESS_THAN_EQUAL_OPERATOR,
+  lte,
+} from "@/builder/nodes/comparison/lte.node.js";
 import { value } from "@/builder/nodes/value.node.js";
 import { DATATYPE } from "@/types.js";
 import { describe, expect, it } from "vitest";
 
 describe("lte", () => {
-  it("should create a comparison node with operator <=", () => {
+  it(`should create a comparison node with operator ${LESS_THAN_EQUAL_OPERATOR}`, () => {
     const result = lte(
       value({ type: DATATYPE.FLOAT, data: 1 }),
       value({ type: DATATYPE.FLOAT, data: 2 }),
     );
 
-    expect(result.data.operator).toBe("<=");
-    expect(result.kind).toBe("comparison");
+    expect(result.data.operator).toBe(LESS_THAN_EQUAL_OPERATOR);
+    expect(result.kind).toBe(COMPARISON_KIND);
   });
 });
