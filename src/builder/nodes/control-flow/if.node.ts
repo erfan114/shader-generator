e8f121285e23f941df5da3e2971201013a7301fb
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-08 16:27:34 +0330

    feat: Update `if` and `while` nodes to accept expression and body parameters

--- src/builder/nodes/control-flow/if.node.ts
+++ src/builder/nodes/control-flow/if.node.ts
@@ -1,7 +1,9 @@
 import { NotImplementedError } from "@/errors.js";
+import type { BooleanExpression } from "../logical/common.js";
+import type { ControlFlowBody } from "./common.js";
 
 export const IF_KIND = "if";
 
-export function if_() {
+export function if_(expression: BooleanExpression, body: ControlFlowBody) {
   throw new NotImplementedError();
 }
--- src/builder/nodes/control-flow/while.node.ts
+++ src/builder/nodes/control-flow/while.node.ts
@@ -1,7 +1,9 @@
 import { NotImplementedError } from "@/errors.js";
+import type { BooleanExpression } from "../logical/common.js";
+import type { ControlFlowBody } from "./common.js";
 
 export const WHILE_KIND = "while";
 
-export function while_() {
+export function while_(expression: BooleanExpression, body: ControlFlowBody) {
   throw new NotImplementedError();
 }
