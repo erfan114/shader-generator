28bfe7c0ad0336af3cff1c696573a948a33c3ad6
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-10 10:17:46 +0330

    fix: Refactor `SubtractionNode` to use `SUBTRACTION_KIND` constant for kind property

--- src/builder/nodes/operations/subtraction.node.ts
+++ src/builder/nodes/operations/subtraction.node.ts
@@ -2,17 +2,19 @@ import { builderNode } from "../../node.js";
 import type { OperationNode } from "./common.js";
 import type { AdditiveDatatype } from "./types/additive.type.js";
 
+export const SUBTRACTION_KIND = "subtraction";
+
 export type SubtractionNode<
   L extends AdditiveDatatype,
   R extends AdditiveDatatype,
-> = OperationNode<"subtraction", L, R>;
+> = OperationNode<typeof SUBTRACTION_KIND, L, R>;
 
 export function subtract<
   L extends AdditiveDatatype,
   R extends AdditiveDatatype,
 >(left: L, right: R): SubtractionNode<L, R> {
   return builderNode({
-    kind: "subtraction",
+    kind: SUBTRACTION_KIND,
     data: {
       left,
       right,
