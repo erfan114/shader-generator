0efcab42871d179347e9e7680aaf3dee41704831
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-10 10:17:29 +0330

    fix: Refactor `DivisionNode` to use `DIVISION_KIND` constant for kind property

--- src/builder/nodes/operations/division.node.ts
+++ src/builder/nodes/operations/division.node.ts
@@ -2,17 +2,19 @@ import { builderNode } from "../../node.js";
 import type { OperationNode } from "./common.js";
 import type { MultiplicativeDatatype } from "./types/multiplicative.type.js";
 
+export const DIVISION_KIND = "division";
+
 export type DivisionNode<
   L extends MultiplicativeDatatype,
   R extends MultiplicativeDatatype,
-> = OperationNode<"division", L, R>;
+> = OperationNode<typeof DIVISION_KIND, L, R>;
 
 export function divide<
   L extends MultiplicativeDatatype,
   R extends MultiplicativeDatatype,
 >(left: L, right: R): DivisionNode<L, R> {
   return builderNode({
-    kind: "division",
+    kind: DIVISION_KIND,
     data: {
       left,
       right,
