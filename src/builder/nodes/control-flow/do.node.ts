f15c9ad23dfe56785dde079a6e40856adf619b01
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-10 03:40:30 +0330

    feat: Implement `do_` function with options for expression and body

--- src/builder/nodes/control-flow/do.node.ts
+++ src/builder/nodes/control-flow/do.node.ts
@@ -1,7 +1,19 @@
-import { NotImplementedError } from "@/errors.js";
+import type { BooleanExpression } from "../logical/common.js";
+import type { ControlFlowBody } from "./common.js";
+import { builderNode, type BuilderNode } from "@/builder/node.js";
 
 export const DO_KIND = "do";
 
-export function do_() {
-  throw new NotImplementedError();
+export type DoNodeOptions = {
+  expression: BooleanExpression;
+  body: ControlFlowBody;
+};
+
+export type DoNode = BuilderNode<typeof DO_KIND, DoNodeOptions>;
+
+export function do_(options: DoNodeOptions): DoNode {
+  return builderNode({
+    kind: DO_KIND,
+    data: options,
+  });
 }
