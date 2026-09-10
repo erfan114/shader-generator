05ed0c503e22d557375a29168fd052f54eaeed8c
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-10 10:17:36 +0330

    fix: Refactor `ModulusNode` to use `MODULUS_KIND` constant for kind property

--- src/builder/nodes/operations/modulus.node.ts
+++ src/builder/nodes/operations/modulus.node.ts
@@ -1,11 +1,13 @@
 import { builderNode } from "../../node.js";
 import type { OperationNode } from "./common.js";
 
-export type ModulusNode<L, R> = OperationNode<"modulus", L, R>;
+export const MODULUS_KIND = "modulus";
+
+export type ModulusNode<L, R> = OperationNode<typeof MODULUS_KIND, L, R>;
 
 export function modulo<L, R>(left: L, right: R): ModulusNode<L, R> {
   return builderNode({
-    kind: "modulus",
+    kind: MODULUS_KIND,
     data: {
       left,
       right,
