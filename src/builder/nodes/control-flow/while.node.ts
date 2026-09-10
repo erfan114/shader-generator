9c7982fea41db25433e012d9f52b448cb37e8394
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-10 03:38:07 +0330

    feat: Refactor `while_` function to accept `WhileNodeOptions` and return `WhileNode`

--- src/builder/nodes/control-flow/while.node.ts
+++ src/builder/nodes/control-flow/while.node.ts
@@ -1,9 +1,19 @@
-import { NotImplementedError } from "@/errors.js";
 import type { BooleanExpression } from "../logical/common.js";
 import type { ControlFlowBody } from "./common.js";
+import { builderNode, type BuilderNode } from "@/builder/node.js";
 
 export const WHILE_KIND = "while";
 
-export function while_(expression: BooleanExpression, body: ControlFlowBody) {
-  throw new NotImplementedError();
+export type WhileNodeOptions = {
+  expression: BooleanExpression;
+  body: ControlFlowBody;
+};
+
+export type WhileNode = BuilderNode<typeof WHILE_KIND, WhileNodeOptions>;
+
+export function while_(options: WhileNodeOptions): WhileNode {
+  return builderNode({
+    kind: WHILE_KIND,
+    data: options,
+  });
 }
