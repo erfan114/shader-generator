ab5681bd2a29fb745cf333d36d644caa08487573
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-10 10:17:40 +0330

    fix: Refactor `MultiplicationNode` to use `MULTIPLICATION_KIND` constant for kind property

--- src/builder/nodes/operations/multiplication.node.ts
+++ src/builder/nodes/operations/multiplication.node.ts
@@ -2,17 +2,19 @@ import { builderNode } from "../../node.js";
 import type { OperationNode } from "./common.js";
 import type { MultiplicativeDatatype } from "./types/multiplicative.type.js";
 
+export const MULTIPLICATION_KIND = "multiplication";
+
 export type MultiplicationNode<
   L extends MultiplicativeDatatype,
   R extends MultiplicativeDatatype,
-> = OperationNode<"multiplication", L, R>;
+> = OperationNode<typeof MULTIPLICATION_KIND, L, R>;
 
 export function multiply<
   L extends MultiplicativeDatatype,
   R extends MultiplicativeDatatype,
 >(left: L, right: R): MultiplicationNode<L, R> {
   return builderNode({
-    kind: "multiplication",
+    kind: MULTIPLICATION_KIND,
     data: { left, right },
   });
 }
