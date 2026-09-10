6e65973c4c536af935dbe7920a8ab2cd906e2b15
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-10 09:49:15 +0330

    fix: Set default type for `InputNode` to `Datatype`

--- src/builder/nodes/input.node.ts
+++ src/builder/nodes/input.node.ts
@@ -4,6 +4,9 @@ import { type IONode, io } from "./common.js";
 
 const INPUT_KIND = "input";
 
-export type InputNode<Type extends Datatype> = IONode<typeof INPUT_KIND, Type>;
+export type InputNode<Type extends Datatype = Datatype> = IONode<
+  typeof INPUT_KIND,
+  Type
+>;
 
 export const input = io(INPUT_KIND);
