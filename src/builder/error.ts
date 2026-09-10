d8ebac6b00a38451dde9fefb507500bdb6f32d01
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-10 18:18:22 +0330

    fix: Update error message in `BuilderNodeYieldError` for clarity

--- src/builder/error.ts
+++ src/builder/error.ts
@@ -1,5 +1,5 @@
 export class BuilderNodeYieldError extends Error {
   public constructor() {
-    super("Yielded value must be a BuilderNode");
+    super("Yielded value must be a valid BuilderNode");
   }
 }
