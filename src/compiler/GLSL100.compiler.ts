ddbf4b79928c25030354030d037a8b1125433a6a
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-12 18:00:39 +0330

    feat: Implement `emit` function for `GLSL100Compiler`

--- src/compiler/GLSL100.compiler.ts
+++ src/compiler/GLSL100.compiler.ts
@@ -2,8 +2,26 @@ import { createCompiler } from "./compiler.js";
 
 export const GLSL100Compiler = createCompiler({
   context: {},
-  emit: ({ nodes, emitter }) => {
-    // TODO: Process nodes
+  emit: ({ nodes, emitter, names }) => {
+    for (const node of nodes) {
+      switch (node.kind) {
+        case "input": {
+          emitter.line(`attribute ? ${names.getName(node)}`);
+
+          break;
+        }
+        case "output": {
+          emitter.line(`varying ? ${names.getName(node)}`);
+
+          break;
+        }
+        case "uniform": {
+          emitter.line(`uniform ? ${names.getName(node)}`);
+
+          break;
+        }
+      }
+    }
 
     return emitter.toString();
   },
