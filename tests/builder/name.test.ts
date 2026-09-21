f182fd9a215a543548c0ec254696018ad69ae116
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-21 21:20:06 +0330

    refactor: Update name generator tests to use generate method and correct imports

--- tests/builder/name.test.ts
+++ tests/builder/name.test.ts
@@ -3,35 +3,24 @@ import { describe, expect, it } from "vitest";
 import {
   NAME_GENERATOR_NAMING_PREFIX,
   createNameGenerator,
-} from "@/builder/name.js";
+} from "@/compiler/name.js";
 
 describe("Name generator", () => {
   it("Generate unique names", () => {
     const nameGenerator = createNameGenerator();
 
-    const lastTenNamesArray = nameGenerator.take(99999).toArray();
-    const lastTenNamesSet = new Set(lastTenNamesArray);
+    const namesArray = Array.from({ length: 9999 }).map(() =>
+      nameGenerator.generate(),
+    );
+    const namesSet = new Set(namesArray);
 
-    expect(lastTenNamesArray).toHaveLength(lastTenNamesSet.size);
+    expect(namesArray).toHaveLength(namesSet.size);
   });
 
   it("Skip 10 names", () => {
     const nameGenerator = createNameGenerator();
 
-    expect(nameGenerator.next().value).toBe(`${NAME_GENERATOR_NAMING_PREFIX}0`);
-    expect(nameGenerator.next().value).toBe(`${NAME_GENERATOR_NAMING_PREFIX}1`);
-
-    // The next generated name advances by 10.
-    expect(nameGenerator.next(10).value).toBe(
-      `${NAME_GENERATOR_NAMING_PREFIX}b`,
-    );
-  });
-
-  it("should throw for negative offsets", () => {
-    const nameGenerator = createNameGenerator();
-
-    nameGenerator.next();
-
-    expect(() => nameGenerator.next(0)).toThrow(RangeError);
+    expect(nameGenerator.generate()).toBe(`${NAME_GENERATOR_NAMING_PREFIX}0`);
+    expect(nameGenerator.generate()).toBe(`${NAME_GENERATOR_NAMING_PREFIX}1`);
   });
 });
