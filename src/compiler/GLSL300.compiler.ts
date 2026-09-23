df0704dd6cf51ae8248e32b5e12a21b130d681cf
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-23 17:56:00 +0330

    feat: Implement function node handling in GLSL300Compiler with argument parsing

--- src/compiler/GLSL300.compiler.ts
+++ src/compiler/GLSL300.compiler.ts
@@ -1,5 +1,6 @@
 import { DATATYPE, type Datatype } from "@/types.js";
 import { createCompiler } from "./compiler.js";
+import { runFunctionNode } from "./helpers/function.helper.js";
 
 const DATATYPE_MAP = {
   [DATATYPE.FLOAT]: "float",
@@ -79,9 +80,23 @@ export const GLSL300Compiler = createCompiler({
           break;
         }
 
-        case "function":
-          // TODO: Complete me
+        case "function": {
+          const args = node.data.args.map((arg) => {
+            const name = names.getName(arg);
+            const type = context.datatypeParser(arg.data.type);
+
+            return `${type} ${name}`;
+          });
+
+          const functionHeader = `(${args.join(", ")})`;
+          const bodyNodes = runFunctionNode(node);
+
+          emitter.block(functionHeader, () => {
+            // TODO: Handle each type
+          });
+
           break;
+        }
 
         default:
           throw new Error(`Unhandled node in GLSL300: ${node satisfies never}`);
