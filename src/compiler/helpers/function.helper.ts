93a09a5a510d065108551068b3618e097a4e6316
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-23 17:55:52 +0330

    feat: Add runFunctionNode helper for executing FunctionNode instances

---
+++ src/compiler/helpers/function.helper.ts
@@ -0,0 +1,21 @@
+import type { BuilderNode } from "@/builder/node.js";
+import type { ArgumentNode } from "@/builder/nodes/argument.node.js";
+import type { FunctionNode } from "@/builder/nodes/function.node.js";
+import type { ValueDatatype } from "@/builder/nodes/value.node.js";
+
+export function runFunctionNode(
+  node: FunctionNode<ArgumentNode[], ValueDatatype | null>,
+): BuilderNode[] {
+  const nodes: BuilderNode[] = [];
+  const instance = node.data.body(...node.data.args);
+
+  let current = instance.next();
+
+  while (!current.done) {
+    nodes.push(current.value);
+
+    current = instance.next(current.value);
+  }
+
+  return nodes;
+}
