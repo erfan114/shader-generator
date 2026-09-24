5cb9a47775744dd1fcd4b27925ffe952c03fdf10
Erfan Hafezi <106922298+erfan114@users.noreply.github.com>
2026-09-24 10:00:07 +0330

    refactor: restructure Parser type for improved clarity and type mapping

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -1,25 +1,44 @@
+import type { InputNode } from "@/builder/nodes/input.node.js";
 import type { CompilerContext } from "./context.js";
+import type { OutputNode } from "@/builder/nodes/output.node.js";
+import type { UniformNode } from "@/builder/nodes/uniform.node.js";
+import type { FunctionNode } from "@/builder/nodes/function.node.js";
+import type { VariableNode } from "@/builder/nodes/variable.node.js";
+import type { DoNode } from "@/builder/nodes/control-flow/do.node.js";
+import type { ForNode } from "@/builder/nodes/control-flow/for.node.js";
+import type { IfNode } from "@/builder/nodes/control-flow/if.node.js";
+import type { SwitchNode } from "@/builder/nodes/control-flow/switch.node.js";
+import type { WhileNode } from "@/builder/nodes/control-flow/while.node.js";
+import type { BreakNode } from "@/builder/nodes/jump/break.node.js";
+import type { ContinueNode } from "@/builder/nodes/jump/continue.node.js";
+import type { DiscardNode } from "@/builder/nodes/jump/discard.node.js";
+import type { ReturnNode } from "@/builder/nodes/jump/return.node.js";
+import type { ScopeNode } from "@/builder/nodes/scope.node.js";
+import type { BuilderNode } from "@/builder/node.js";
 
-const PARSER_FIELDS = [
-  "input",
-  "output",
-  "uniform",
-  "function",
-  "variable",
-  "do",
-  "for",
-  "if",
-  "switch",
-  "while",
-  "break",
-  "continue",
-  "discard",
-  "return",
-  "scope",
-] as const;
+type ParserFieldsMap = {
+  input: InputNode;
+  output: OutputNode;
+  uniform: UniformNode;
+  function: FunctionNode;
+  variable: VariableNode;
+  do: DoNode;
+  for: ForNode;
+  if: IfNode;
+  switch: SwitchNode;
+  while: WhileNode;
+  break: BreakNode;
+  continue: ContinueNode;
+  discard: DiscardNode;
+  return: ReturnNode;
+  scope: ScopeNode;
+};
 
-type ParserFields = (typeof PARSER_FIELDS)[number];
+export type ParserFunction<T extends BuilderNode> = (
+  context: CompilerContext,
+  node: T,
+) => string;
 
-export type ParserFunction = (context: CompilerContext) => string;
-
-export type Parser = Record<ParserFields, ParserFunction>;
+export type Parser = {
+  [key in keyof ParserFieldsMap]: ParserFunction<ParserFieldsMap[key]>;
+};
